25c56e18a936dc73140e3ff4e9bf23311d141299
menyaboo <tosha.kolmay@gmail.com>
2024-01-27 06:25:34 +0700

    add role in user model to index function

--- app/Http/Request/Auth/RegisterRequest.php
+++ app/Http/Request/Auth/RegisterRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Request\Auth;
 
 use App\Http\Request\ApiRequest;
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
 class RegisterRequest extends ApiRequest
